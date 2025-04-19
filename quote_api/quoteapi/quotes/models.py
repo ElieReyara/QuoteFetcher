d6f65664c953165a153f1422a75cf54080570068
Elie <reyaraelie0@gmail.com>
2025-04-19 22:44:05 +0000

    Add Quote model and migrations

--- quote_api/quoteapi/quotes/models.py
+++ quote_api/quoteapi/quotes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Quote(models.Model):
+    text = models.CharField(max_length=1000)
+    author = models.CharField(max_length=100, blank=True, null=True)
+    category = models.CharField(max_length=100, blank=True, null=True)
