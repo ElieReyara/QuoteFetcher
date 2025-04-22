0861f11e0421bd8b1e76683b823a1637a170f75c
Elie <reyaraelie0@gmail.com>
2025-04-22 18:58:36 +0000

    Fix saved-quotes with unique authors list

--- quote_api/quoteapi/quotes/urls.py
+++ quote_api/quoteapi/quotes/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("quote/", views.quote, name="quote"),
-    path("random/", views.random, name="random")
+    path("random/", views.random, name="random"),
+    path("savedQuotes/", views.savedQuotes, name="savedQuotes")
 ]
--- quote_api/quoteapi/quotes/views.py
+++ quote_api/quoteapi/quotes/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 import requests
 import os
 from dotenv import find_dotenv,load_dotenv
+from quotes.models import Quote
 
 #Il va retrouver le fichier .env
 dotenv_path = find_dotenv()
@@ -23,8 +24,22 @@ def quote(request):
         response = requests.get(url, headers={'X-Api-Key': API_KEY})
         if response.status_code == 200:
             data = response.json()
+            #On cree un objet (enregistrement) et on le sauvegarde dans la base
+            quote = Quote(text=data[0]['quote'], author=data[0]['author'], category=data[0]['category'])
+            quote.save()
             return JsonResponse({'quote': data[0]['quote'], 'author': data[0]['author'], 'category': data[0]['category']})
         else:
             return JsonResponse({'error': f"API error: {response.status_code}"})
     except requests.RequestException as e:
         return JsonResponse({'error':str(e)})
+
+def savedQuotes(request):
+    #objects etant le manager du model
+    querySet = Quote.objects.all().values('text', 'author', 'category')
+    authorSeen = Quote.objects.values('author').distinct()
+    if querySet.exists():
+        allQuote = list(querySet)
+        authorSeen = [item['author'] for item in authorSeen]
+        return JsonResponse({'quotes':allQuote, 'authors':authorSeen}, safe=False)
+    else:
+        return JsonResponse({'message':f"Pas encore de citation sauvegarde"})
