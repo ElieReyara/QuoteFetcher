ba7ff707d10a5123dc4ad4b9c5083daaed8e5691
Elie <reyaraelie0@gmail.com>
2025-07-17 10:20:45 +0000

    Ajout fonction logout et amelioration front login

--- quote_api/quoteapi/accounts/urls.py
+++ quote_api/quoteapi/accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
